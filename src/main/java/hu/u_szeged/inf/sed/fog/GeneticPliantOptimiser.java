6e96bf8d40e1a711d32032898af10d3f348bd5d2
bastyha <szentesidominik@gmail.com>
2025-03-12 12:30:26 +0100

    Run simulator as a process, instead of directly using it

--- src/main/java/hu/u_szeged/inf/sed/fog/GeneticPliantOptimiser.java
+++ src/main/java/hu/u_szeged/inf/sed/fog/GeneticPliantOptimiser.java
@@ -1,25 +1,59 @@
 package hu.u_szeged.inf.sed.fog;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import io.jenetics.*;
 import io.jenetics.engine.Engine;
 import io.jenetics.engine.EvolutionResult;
 import io.jenetics.util.Factory;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 
 public class GeneticPliantOptimiser {
     public static double fitness(final Genotype<DoubleGene> entity){
-        double price = entity.get(0).gene().allele();
-        System.out.println("nume: "+price);
+        double fitness = 0.0;
+        double param = entity.gene().allele();
         try {
-            var res = FogExample.sim(price);
-            var cost = res.getCost().getTotalCost();
-            return cost;
+            Process process = new ProcessBuilder(
+                    "java",
+                    "-cp",
+                    "dissect-cf-fog-1.0.0-SNAPSHOT-jar-with-dependencies.jar",
+                    "hu.u_szeged.inf.fog.simulator.demo.IoTSimulation",
+                    "LPDS_original.xml",
+                    Double.toString(param))
+                    .directory(new File("src/main/resources"))
+                    .start();
 
-        }catch (Exception e){
+            // if i don't read the error stream, the application won't run
+            process.getErrorStream().readAllBytes();
+            // get output of program az string
+            String text = new String(process.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+
+            // get last line of output
+            var split_text = text.split("\n");
+            var object = split_text[split_text.length-1];
+
+            // convert last line of output to a *very* generic map
+            // TODO: create a class which the output HAS to match
+            TypeToken<Map<Object, Object>> mapType = new TypeToken<>(){};
+            Map<Object, Object> objectObjectMap = new Gson().fromJson(object, mapType);
+
+            // extract fitness from output
+            fitness = (double) ((Map<Object, Object>) objectObjectMap.get("cost")).get("totalCost");
+
+            // wait for the process to finish
+            process.waitFor();
+
+            System.out.printf("%f -> %f%n", param, fitness);
+        }catch (Exception e) {
             e.printStackTrace();
         }
-        return  1;
+        return fitness;
+
     }
 
     public static void evolution(){
@@ -36,7 +70,7 @@ public class GeneticPliantOptimiser {
                         new MeanAlterer<>(0.2)
                 )
                 .minimizing()
-                .executor(Executors.newSingleThreadExecutor()) // to execute all the fitness functions on the main thread
+//                .executor(Executors.newSingleThreadExecutor()) // to execute all the fitness functions on the main thread
                 .build();
         var result = engine
                 .stream()
--- src/main/java/hu/u_szeged/inf/sed/fog/Main.java
+++ src/main/java/hu/u_szeged/inf/sed/fog/Main.java
@@ -1,9 +1,12 @@
 package hu.u_szeged.inf.sed.fog;
 
-import io.jenetics.*;
-import io.jenetics.engine.Engine;
-import io.jenetics.engine.EvolutionResult;
-import io.jenetics.util.Factory;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
